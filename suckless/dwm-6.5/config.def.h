fdc1dc7d48ad6d66a27a0bdfa99c65cd6b213697
samstevens127 <samuelstevens03@gmail.com>
2024-05-29 13:27:39 -0400

    updated dwm config

--- suckless/dwm-6.5/config.def.h
+++ suckless/dwm-6.5/config.def.h
@@ -12,6 +12,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char col_purple[]	    = "#7a49a5";
+static const char col_yellow[]	    = "#ffff00";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +22,7 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_purple,  col_yellow  },
 };
 
 /* tagging */
