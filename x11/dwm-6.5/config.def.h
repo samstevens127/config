46d63673f44dac303595897b977bae4fc6a3a04e
samstevens127 <samuelstevens03@gmail.com>
2024-06-10 10:24:17 -0400

    fixed dmenu color

--- x11/dwm-6.5/config.def.h
+++ x11/dwm-6.5/config.def.h
@@ -68,7 +68,7 @@ static const char *webbrowser[]    = {"firefox",					NULL};
 static const char *screenlocker[]  = {"slock",						NULL};
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
